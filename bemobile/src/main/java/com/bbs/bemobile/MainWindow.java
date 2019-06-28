eb3bc42fb7eea725c73298736527199e7817b885
DEEP THOUGHT v2 [EARTH] <DEEP@THOUGHTS.question>
2019-06-28 10:14:55 +0200

    Link button to starforce

--- bemobile/src/main/java/com/bbs/bemobile/MainWindow.java
+++ bemobile/src/main/java/com/bbs/bemobile/MainWindow.java
@@ -1,6 +1,7 @@
 package com.bbs.bemobile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,5 +19,16 @@ public class MainWindow extends AppCompatActivity {
         StaticValues.playSound(this, R.raw.btn_tick);
         Intent in = new Intent(this, CassaActivity.class);
         startActivity(in);
+    }
+
+    public void starforceClick(View view)
+    {
+        goToUrl("http://starforce.masterbit.net");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
